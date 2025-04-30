2b0a88835c8be8b83093601748d8c2c7f62ff537
João Lucas <155165579+JoaoLlucas2@users.noreply.github.com>
2025-04-30 10:28:17 -0300

    final confs and template html for the 'details.component'

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } 
 from "../housing-location/housing-location.component";
 import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -25,4 +26,10 @@ import { HousingLocation } from '../housing-location';
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.
+    getAllHousingLocations();
+  }
 }
